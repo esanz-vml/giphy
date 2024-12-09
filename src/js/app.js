404871aebb0153352fb0869043df5cfe02cf2607
Eduard Sanz <neoavangion@gmail.com>
2024-12-09 09:49:18 +0000

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import config from "./config.js";
+
+console.log("Hello from app.js");
+console.log("GIPHY_API_URL: ", config.GIPHY_API_URL);
---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+const config = {
+    GIPHY_API_URL: "https://api.giphy.com/v1/gifs/",
+    GIPHY_API_KEY: "HvYgkM25Af1yLkVVM1SQ353xdH7G6z08",
+    GIPHY_API_LIMIT: 20,
+    GIPHY_API_RATING: "G"
+}
+
+export default config;
